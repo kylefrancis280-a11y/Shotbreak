ff6081c7d09090cbec5a4f43548ccb91546fff07
kylefrancis280-a11y <kylefrancis280@gmail.com>
2026-05-24 12:40:12 -0400

    fix: hardcode shotbreak firebase config as fallbacks in get-config.js

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -1,27 +1,17 @@
 exports.handler = async () => {
   const cfg = {
-    apiKey:            process.env.FIREBASE_API_KEY,
-    authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL:       process.env.FIREBASE_DATABASE_URL,
-    projectId:         process.env.FIREBASE_PROJECT_ID,
-    storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:             process.env.FIREBASE_APP_ID,
-    wavespeedApiKey:   process.env.WAVESPEED_API_KEY
+    apiKey:            process.env.FIREBASE_API_KEY            || "AIzaSyA5-NRXzzkWuGafQ5-EukGF9WMnQ2txFFA",
+    authDomain:        process.env.FIREBASE_AUTH_DOMAIN        || "shotbreak-9f342.firebaseapp.com",
+    databaseURL:       process.env.FIREBASE_DATABASE_URL       || "https://shotbreak-9f342-default-rtdb.firebaseio.com",
+    projectId:         process.env.FIREBASE_PROJECT_ID         || "shotbreak-9f342",
+    storageBucket:     process.env.FIREBASE_STORAGE_BUCKET     || "shotbreak-9f342.firebasestorage.app",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "515766987392",
+    appId:             process.env.FIREBASE_APP_ID             || "1:515766987392:web:ac3644d952c69d11c7d465",
+    wavespeedApiKey:   process.env.WAVESPEED_API_KEY           || ""
   };
-  const missing = Object.entries(cfg)
-    .filter(([k, v]) => !v && k !== 'wavespeedApiKey')
-    .map(([k]) => k);
-  if (missing.length) {
-    return {
-      statusCode: 500,
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: 'Missing env vars: ' + missing.join(', ') })
-    };
-  }
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify(cfg)
   };
 };
